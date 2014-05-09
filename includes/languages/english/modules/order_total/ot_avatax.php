814e162cc82b98fd3e4af89a5405fa02df944cca
Loke Uei <lokeuei@hotmail.com>
2014-05-09 13:57:46 -0700

    first upload of all OSCommerce Source files

---
+++ includes/languages/english/modules/order_total/ot_avatax.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * ot_avatax.php language - order-total module
+ *
+ * @package orderTotal
+ * @copyright Copyright 2014 Avalara, Inc.
+ * @version 2.3.R 2014-01-06 16:00:00Z ajeh $
+ */
+/**
+ * osCommerce Connector for AvaTax
+ *
+ */
+
+  define('MODULE_ORDER_TOTAL_AVATAX_TITLE', 'AvaTax');
+  define('MODULE_ORDER_TOTAL_AVATAX_DESCRIPTION', 'osCommerce Connector for AvaTax');
+  define('TEXT_INFO_AVATAX', '<strong>Note:</strong>Sales Tax by Avalara, Inc');
